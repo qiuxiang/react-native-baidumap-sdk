d227d41c8a06f9a5d5030f941a392e13370184e6
7c00 <i@7c00.cc>
2018-01-16 03:19:05 +0800

    Fix

---
+++ example/src/examples/location/re-geocode.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Location } from 'react-native-baidumap-sdk'
+
+const style = StyleSheet.create({
+  body: {
+    padding: 16,
+  },
+  item: {
+    flexDirection: 'row',
+    marginBottom: 2,
+  },
+  label: {
+    color: '#f5533d',
+    width: 120,
+  },
+})
+
+export default class ReGeoCode extends Component {
+  static navigationOptions = { title: 'ReGeocode' }
+
+  state = {}
+
+  componentDidMount() {
+    Location.setOptions({
+      gps: true,
+      auto: true,
+      reGeocode: true,
+    })
+    Location.request()
+    this.listener = Location.addLocationListener(location => this.setState(location))
+  }
+
+  componentWillUnmount() {
+    Location.stop()
+    this.listener.remove()
+  }
+
+  render() {
+    return (
+      <View style={style.body}>
+        {Object.keys(this.state).map(key => (
+          <View style={style.item} key={key}>
+            <Text style={style.label}>{key}</Text>
+            <Text>{this.state[key]}</Text>
+          </View>
+        ))}
+      </View>
+    )
+  }
+}
