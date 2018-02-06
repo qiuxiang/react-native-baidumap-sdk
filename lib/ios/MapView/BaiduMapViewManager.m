ec08f7593376de283f3b196868b66150bb8ac9b5
7c00 <i@7c00.cc>
2018-02-06 22:30:40 +0800

    Add iOS MapView#setStatus

--- lib/ios/MapView/BaiduMapViewManager.m
+++ lib/ios/MapView/BaiduMapViewManager.m
@@ -40,4 +40,32 @@ RCT_EXPORT_VIEW_PROPERTY(onBaiduMapLongClick, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onBaiduMapDoubleClick, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onBaiduMapStatusChange, RCTBubblingEventBlock)
 
+RCT_EXPORT_METHOD(setStatus:(nonnull NSNumber *)reactTag params:(NSDictionary *)params duration:(int)duration) {
+    [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+        BaiduMapView *mapView = (BaiduMapView *) viewRegistry[reactTag];
+        BMKMapStatus *mapStatus = [BMKMapStatus new];
+        
+        if (params[@"zoomLevel"]) {
+            mapStatus.fLevel = [params[@"zoomLevel"] floatValue];
+        }
+        
+        if (params[@"center"]) {
+            NSDictionary *coordinate = params[@"center"];
+            mapStatus.targetGeoPt = CLLocationCoordinate2DMake(
+                    [coordinate[@"latitude"] doubleValue],
+                    [coordinate[@"longitude"] doubleValue]);
+        }
+        
+        if (params[@"overlook"]) {
+            mapStatus.fOverlooking = [params[@"overlook"] floatValue];
+        }
+        
+        if (params[@"rotation"]) {
+            mapStatus.fRotation = [params[@"rotation"] floatValue];
+        }
+        
+        [mapView setMapStatus:mapStatus withAnimation:true withAnimationTime:duration];
+    }];
+}
+
 @end
