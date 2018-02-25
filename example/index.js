3a448f1d3e5a4a89c36ef109e649e9090499f7f4
7c00 <i@7c00.cc>
2018-02-25 23:19:50 +0800

    Add SDK module, close #11

--- example/index.js
+++ example/index.js
@@ -1,9 +1,10 @@
-// @flow
 import { StatusBar } from 'react-native'
 import { StackNavigator } from 'react-navigation'
+import { SDK } from 'react-native-baidumap-sdk'
 import examples from './examples'
 
 StatusBar.setBarStyle('light-content')
+SDK.init()
 
 export default StackNavigator(examples, {
   navigationOptions: {
