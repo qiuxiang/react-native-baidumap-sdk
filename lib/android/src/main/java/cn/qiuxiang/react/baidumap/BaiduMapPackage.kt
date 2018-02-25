73966cc2c8ac46f0947f4c2ea2b793f2f3bfaa3c
7c00 <i@7c00.cc>
2018-02-25 23:19:50 +0800

    Add SDK module, close #11

--- lib/android/src/main/java/cn/qiuxiang/react/baidumap/BaiduMapPackage.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/baidumap/BaiduMapPackage.kt
@@ -8,7 +8,11 @@ import com.facebook.react.uimanager.ViewManager
 
 class BaiduMapPackage : ReactPackage {
     override fun createNativeModules(context: ReactApplicationContext): List<NativeModule> {
-        return listOf(BaiduMapLocationModule(context), BaiduMapGeocodeModule(context))
+        return listOf(
+                BaiduMapLocationModule(context),
+                BaiduMapGeocodeModule(context),
+                BaiduMapSDKModule(context)
+        )
     }
 
     override fun createViewManagers(context: ReactApplicationContext): List<ViewManager<*, *>> {
