e5036b9869e87d02b8cdd98be20c083723f0e68a
7c00 <i@7c00.cc>
2018-04-14 15:52:44 +0800

    Fix #30

--- example/examples/overlays/index.js
+++ example/examples/overlays/index.js
@@ -4,12 +4,10 @@ import Polyline from './polyline'
 import Polygon from './polygon'
 import Circle from './circle'
 import HeatMap from './heat-map'
-import Text from './text'
 
 export default mapComponents('overlays', {
   Polyline,
   Polygon,
   Circle,
   HeatMap,
-  Text,
 })
