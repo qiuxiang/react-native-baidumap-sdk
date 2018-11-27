d1c3f56489fd5b6a28de94f09faba1767d7f088d
7c00 <i@7c00.cc>
2018-11-27 13:49:51 +0800

    Upgrade RN to v0.57

--- android/app/src/main/java/com/rnbaidumap/MainActivity.java
+++ android/app/src/main/java/com/rnbaidumap/MainActivity.java
@@ -3,6 +3,7 @@ package com.rnbaidumap;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
